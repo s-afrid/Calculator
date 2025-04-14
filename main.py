bd8b99727cd9f632a3c6af4a735a11d9127f469d
syed-afrid <afrid0052@gmail.com>
2025-04-14 22:25:54 +0530

    Equation label

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ from tkinter import *
 window = Tk()
 window.title("Calculator")
 window.geometry("500x500")
+
+equation_text = ""
+equation_label = StringVar()
+
+label = Label(window, textvariable=equation_label, 
+              font=("consolas", 20), bg="white", 
+              width=24, height=2, 
+              )
+label.pack()
+
 window.mainloop()
