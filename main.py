5d3e493f3b8b0ff2df38074258ee1b8db439356b
syed-afrid <afrid0052@gmail.com>
2025-04-15 22:54:27 +0530

    Numeric buttons

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
+def button_press(number):
+    pass
+
 window = Tk()
 window.title("Calculator")
 window.geometry("500x500")
@@ -12,5 +15,46 @@ label = Label(window, textvariable=equation_label,
               width=24, height=2, 
               )
 label.pack()
+
+# Frame to hold button
+
+frame = Frame(window)
+frame.pack()
+
+# All buttons
+
+button1 = Button(frame, text=1,height=3,width=3,
+                 font=35,command=lambda: button_press(1))
+button1.grid(row=0,column=0)
+button2 = Button(frame, text=2,height=3,width=3,
+                 font=35,command=lambda: button_press(2))
+button2.grid(row=0,column=1)
+button3 = Button(frame, text=3,height=3,width=3,
+                 font=35,command=lambda: button_press(3))
+button3.grid(row=0,column=2)
+
+button4 = Button(frame, text=4,height=3,width=3,
+                 font=35,command=lambda: button_press(4))
+button4.grid(row=1,column=0)
+button5 = Button(frame, text=5,height=3,width=3,
+                 font=35,command=lambda: button_press(5))
+button5.grid(row=1,column=1)
+button6 = Button(frame, text=6,height=3,width=3,
+                 font=35,command=lambda: button_press(6))
+button6.grid(row=1,column=2)
+
+button7 = Button(frame, text=7,height=3,width=3,
+                 font=35,command=lambda: button_press(7))
+button7.grid(row=2,column=0)
+button8 = Button(frame, text=8,height=3,width=3,
+                 font=35,command=lambda: button_press(8))
+button8.grid(row=2,column=1)
+button9 = Button(frame, text=9,height=3,width=3,
+                 font=35,command=lambda: button_press(9))
+button9.grid(row=2,column=2)
+
+button0 = Button(frame, text=0,height=3,width=3,
+                 font=35,command=lambda: button_press(0))
+button0.grid(row=3,column=1)
 
 window.mainloop()
