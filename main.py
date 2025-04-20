1856cf88ee191851b5eb5588ff362ac8f53ab1cb
syed-afrid <afrid0052@gmail.com>
2025-04-20 22:49:18 +0530

    Display equation

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from tkinter import *
 
 def button_press(number):
-    pass
+    global equation_text
+    equation_text = equation_text + str(number)
+    equation_label.set(equation_text)
 
 def clear():
     pass
@@ -11,7 +13,7 @@ def equals():
 
 window = Tk()
 window.title("Calculator")
-window.geometry("500x500")
+window.geometry("800x800")
 
 equation_text = ""
 equation_label = StringVar()
@@ -29,64 +31,64 @@ frame.pack()
 
 # All buttons
 
-button1 = Button(frame, text=1,height=3,width=3,
+button1 = Button(frame, text=1,height=4,width=12,
                  font=35,command=lambda: button_press(1))
 button1.grid(row=0,column=0)
-button2 = Button(frame, text=2,height=3,width=3,
+button2 = Button(frame, text=2,height=4,width=12,
                  font=35,command=lambda: button_press(2))
 button2.grid(row=0,column=1)
-button3 = Button(frame, text=3,height=3,width=3,
+button3 = Button(frame, text=3,height=4,width=12,
                  font=35,command=lambda: button_press(3))
 button3.grid(row=0,column=2)
 
-button4 = Button(frame, text=4,height=3,width=3,
+button4 = Button(frame, text=4,height=4,width=12,
                  font=35,command=lambda: button_press(4))
 button4.grid(row=1,column=0)
-button5 = Button(frame, text=5,height=3,width=3,
+button5 = Button(frame, text=5,height=4,width=12,
                  font=35,command=lambda: button_press(5))
 button5.grid(row=1,column=1)
-button6 = Button(frame, text=6,height=3,width=3,
+button6 = Button(frame, text=6,height=4,width=12,
                  font=35,command=lambda: button_press(6))
 button6.grid(row=1,column=2)
 
-button7 = Button(frame, text=7,height=3,width=3,
+button7 = Button(frame, text=7,height=4,width=12,
                  font=35,command=lambda: button_press(7))
 button7.grid(row=2,column=0)
-button8 = Button(frame, text=8,height=3,width=3,
+button8 = Button(frame, text=8,height=4,width=12,
                  font=35,command=lambda: button_press(8))
 button8.grid(row=2,column=1)
-button9 = Button(frame, text=9,height=3,width=3,
+button9 = Button(frame, text=9,height=4,width=12,
                  font=35,command=lambda: button_press(9))
 button9.grid(row=2,column=2)
 
-button0 = Button(frame, text=0,height=3,width=3,
+button0 = Button(frame, text=0,height=4,width=12,
                  font=35,command=lambda: button_press(0))
 button0.grid(row=3,column=1)
 
 # Symbols
 
-plus = Button(frame, text="+",height=3,width=3,
+plus = Button(frame, text="+",height=4,width=12,
                  font=35,command=lambda: button_press("+"))
 plus.grid(row=0,column=3)
-minus = Button(frame, text="-",height=3,width=3,
+minus = Button(frame, text="-",height=4,width=12,
                  font=35,command=lambda: button_press("-"))
 minus.grid(row=1,column=3)
-multiply = Button(frame, text="*",height=3,width=3,
+multiply = Button(frame, text="*",height=4,width=12,
                  font=35,command=lambda: button_press("*"))
 multiply.grid(row=2,column=3)
-divide = Button(frame, text="/",height=3,width=3,
+divide = Button(frame, text="/",height=4,width=12,
                  font=35,command=lambda: button_press("/"))
 divide.grid(row=3,column=3)
-equal = Button(frame, text="=",height=3,width=3,
+equal = Button(frame, text="=",height=4,width=12,
                  font=35,command=lambda: button_press("="))
 equal.grid(row=3,column=2)
-decimal = Button(frame, text=".",height=3,width=3,
+decimal = Button(frame, text=".",height=4,width=12,
                  font=35,command=lambda: button_press("."))
 decimal.grid(row=3,column=0)
 
 # Clear button
 
-Clear = Button(window, text="clear",height=3,width=3,
+Clear = Button(window, text="clear",height=4,width=12,
                  font=35,command=clear)
 Clear.pack()
 
